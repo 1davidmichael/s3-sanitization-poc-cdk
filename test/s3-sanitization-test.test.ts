e20b617507b2a58f0dd3b9753adc735171f35f46
David Michael <1.david.michael@gmail.com>
2022-04-07 13:30:05 -0500

    Add cdk test

--- test/s3-sanitization-test.test.ts
+++ test/s3-sanitization-test.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as S3SanitizationTest from '../lib/s3-sanitization-test-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as S3SanitizationTest from '../lib/s3-sanitization-test-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/s3-sanitization-test-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+test('Lambda Created', () => {
+  const app = new cdk.App();
 //     // WHEN
-//   const stack = new S3SanitizationTest.S3SanitizationTestStack(app, 'MyTestStack');
+  const stack = new S3SanitizationTest.S3SanitizationTestStack(app, 'MyTestStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    // VisibilityTimeout: 300
+  });
 });
